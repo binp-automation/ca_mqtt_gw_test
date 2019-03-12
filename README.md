c2946dcc3ce095937fe141ece415818857e2ed11
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-03-12 16:28:54 +0700

    unittesting

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 [travis_badge]: https://api.travis-ci.org/binp-automation/ca_mqtt_gw_test.svg
 [license_badge]: https://img.shields.io/github/license/binp-automation/ca_mqtt_gw_test.svg
 
-[travis]: https://travis-ci.org/nthend/ringbuf
+[travis]: https://travis-ci.org/binp-automation/ca_mqtt_gw_test
 [license]: https://github.com/binp-automation/ca_mqtt_gw_test/blob/master/LICENSE
 
 ## Required packages
--- script/test_all.py
+++ script/test_all.py
@@ -3,18 +3,25 @@
 import os
 from subprocess import run
 
-def test(path, name):
-	print("\n*** %s TEST ***" % name)
-	if run(["python3", path]).returncode == 0:
+def check(result):
+	if result.returncode == 0:
 		print("*** PASSED ***\n")
 		return True
 	else:
 		print("!!! FAILED !!!\n")
 		return False
 
+def unittest():
+	print("\n*** UNITTEST ***")
+	return check(run(["bash", "-c", "./script/unittest.sh"]))
+
+def test(path, name):
+	print("\n*** %s TEST ***" % name)
+	return check(run(["python3", path]))
+
 tests = [("./script/test_%s.py" % t, t.upper()) for t in ["ca", "mqtt", "ca_mqtt_gw"]]
 
-if all([test(p, n) for p, n in tests]):
+if all([unittest()] + [test(p, n) for p, n in tests]):
 	print("\n*** ALL TESTS PASSED ***")
 	exit(0)
 else:
